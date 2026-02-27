de2edbdcf6d7b8e0320e36eea50bf3f02a9464e3
jonatangarzon <jonatangarzon@colegioguanenta.edu.co>
2026-02-27 09:57:58 -0500

    jonatangarzon

---
+++ prsentacion-while.py
@@ -0,0 +1,14 @@
+# toda la presentacion aqui >13c
+#inicio
+
+bienvenido =
+
+print(bienvenido)
+N = 0
+
+while N <= 100
+
+    print(N)
+    N +=1
+
+print("Oh no, superaste el limite de dijitos!")
